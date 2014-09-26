bd4672bae6b9aa735c62714ea5d9daeca68f2ae2
Jan Christoph Ebersbach <jceb@e-jc.de>
2014-09-26 17:04:57 +0200

    update script

--- mutt/list_imap_mailboxes.py
+++ mutt/list_imap_mailboxes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
 """
@@ -13,33 +13,33 @@ import sys
 from optparse import OptionParser
 from ConfigParser import ConfigParser
 
-def extractMailboxes (l):
-	return [ m.split('"')[-2] for m in l ]
+def extractMailboxes(l):
+	return [ m.split(' "/" ')[-1].strip('"') for m in l ]
 
-def createOptionParser ():
-	parser = OptionParser ()
+def createOptionParser():
+	parser = OptionParser()
 	parser.usage = "usage: %prog [options] host"
-	parser.add_option ("-F", "--passwdfile", dest="passwdfile", help="Password file")
-	parser.add_option ("-U", "--username", dest="username", help="Username")
-	parser.add_option ("-P", "--password", dest="password", help="Password")
-	parser.add_option ("-p", "--port", type='int', dest="port", help="Port (default: %default)")
-	parser.add_option ("-l", dest="l", help="Leading string (default: %default)")
-	parser.add_option ("--ssl", action="store_true", dest="ssl", help="Enable SSL (default: %default)")
+	parser.add_option("-F", "--passwdfile", dest="passwdfile", help="Password file")
+	parser.add_option("-U", "--username", dest="username", help="Username")
+	parser.add_option("-P", "--password", dest="password", help="Password")
+	parser.add_option("-p", "--port", type='int', dest="port", help="Port(default: %default)")
+	parser.add_option("-l", dest="l", help="Leading string(default: %default)")
+	parser.add_option("--ssl", action="store_true", dest="ssl", help="Enable SSL(default: %default)")
 
-	parser.set_defaults (host='localhost', ssl=False, username='', password='', l='+')
+	parser.set_defaults(host='localhost', ssl=False, username='', password='', l='+')
 
 	return parser
 
 if __name__ == '__main__':
-	parser = createOptionParser ()
+	parser = createOptionParser()
 
-	(options, args) = parser.parse_args ()
+	(options, args) = parser.parse_args()
 
 	# finish options
 	if not args:
 		print >> sys.stderr, 'ERROR: No host specified.'
-		parser.print_help ()
-		sys.exit (1)
+		parser.print_help()
+		sys.exit(1)
 
 	imap = imaplib.IMAP4
 	if options.ssl:
@@ -50,35 +50,35 @@ if __name__ == '__main__':
 		options.port = 143
 
 	if options.passwdfile:
-		config = ConfigParser ()
-		config.read (options.passwdfile)
-		options.username = config.get ('User', 'username')
-		options.password = config.get ('User', 'password')
+		config = ConfigParser()
+		config.read(options.passwdfile)
+		options.username = config.get('User', 'username')
+		options.password = config.get('User', 'password')
 
 	if not options.username:
 		print >> sys.stderr, 'ERROR: No username specified.'
-		parser.print_help ()
-		sys.exit (1)
+		parser.print_help()
+		sys.exit(1)
 
 	# open connection
-	con = imap (args[0], options.port)
-	if con.login (options.username, options.password)[0] != 'OK':
+	con = imap(args[0], options.port)
+	if con.login(options.username, options.password)[0] != 'OK':
 		print >> sys.stderr, 'Login failed'
-		sys.exit (1)
-	if con.select ()[0] != 'OK':
+		sys.exit(1)
+	if con.select()[0] != 'OK':
 		print >> sys.stderr, 'Select failed'
-		sys.exit (1)
-	if con.check ()[0] != 'OK':
+		sys.exit(1)
+	if con.check()[0] != 'OK':
 		print >> sys.stderr, 'Check failed'
-		sys.exit (1)
-	boxes = con.list ()
+		sys.exit(1)
+	boxes = con.list()
 	if boxes[0] != 'OK':
 		print >> sys.stderr, 'Failed to retrieve mailboxes'
-		sys.exit (1)
+		sys.exit(1)
 	con.close()
 	con.logout()
 
 	# print mailboxes
-	mailboxes = extractMailboxes (boxes[1])
-	mailboxes.sort ()
-	print '"' + options.l + ('" "%s' % options.l).join (mailboxes) + '"'
+	mailboxes = extractMailboxes(boxes[1])
+	mailboxes.sort()
+	print '"%s"' % (options.l + ('" "%s' % options.l).join(mailboxes))
