9aa48de7070f912ae67f40636ed2345efe2028d7
Jan Christoph Ebersbach <ebersbach@univention.de>
2014-11-07 08:24:35 +0100

    disable cdb

--- zsh/rc/misc.zsh
+++
@@ -1,8 +0,0 @@
-# source cdargs bash binding
-if [[ -e ~/.cdargs-bash.sh ]]; then
-	source ~/.cdargs-bash.sh
-elif [[ -e /usr/share/cdargs/cdargs-bash.sh ]]; then
-	source /usr/share/cdargs/cdargs-bash.sh
-fi
-
-# vi: ft=zsh:tw=0:sw=4:ts=4
