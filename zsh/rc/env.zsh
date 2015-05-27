808d2d8c1cdfeb5637d40b09712f4f8c2e9e74bb
Jan Christoph Ebersbach <jceb@e-jc.de>
2015-05-27 21:03:16 +0200

    update gpg settings

--- zsh/rc/env.zsh
+++ zsh/rc/env.zsh
@@ -11,30 +11,31 @@ fi
 # to get around sorting issues, define differently
 export LC_COLLATE="C"
 
-EDITOR=vim;						export EDITOR
-GIT_EDITOR=/usr/bin/vim			export GIT_EDITOR
-#EMAIL=put me in ~/.zshother;	export EMAIL
-BROWSER=x-www-browser;			export BROWSER
+export EDITOR=vim
+export GIT_EDITOR=/usr/bin/vim
+# export EMAIL=put me in ~/.zshother
+export BROWSER=x-www-browser
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
 # add -F to automatically quit less if page fits on one screen
-LESS="--ignore-case -rSX";		export LESS
-PAGER=less;						export PAGER
-# PAGER=vimpager;						export PAGER
+export LESS="--ignore-case -rSX"
+export PAGER=less
+# export PAGER=vimpager
 
 # export TTY properly to support gnupg
-GPG_TTY=$(tty);					export GPG_TTY
+export GPG_TTY=$(tty)
+export GPG_AGENT_INFO="$HOME/.gnupg/S.gpg-agent"
 
 # quilt settings, always look for patches in the debian/patches directory
-QUILT_PATCHES=debian/patches;	export QUILT_PATCHES
-QUILT_DIFF_ARGS="--color=auto";	export QUILT_DIFF_ARGS
+export QUILT_PATCHES=debian/patches
+export QUILT_DIFF_ARGS="--color=auto"
 
 # bc settings
-BC_ENV_ARGS=~/.bcrc;			export BC_ENV_ARGS
+export BC_ENV_ARGS=~/.bcrc
 
 # go lang settings
-GOPATH="${HOME}/go";			export GOPATH
+export GOPATH="${HOME}/go"
 
 # set PATH so it includes user's private bin if it exists
 for i in "$HOME/.gem/ruby/1.9.1/bin" "$HOME/Documents/toolshed/" "$HOME/.cabal/bin" "$HOME/.local/bin" "$HOME/bin" "$HOME/.gem/ruby/2.1.0/bin" "${GOPATH}/bin"; do
@@ -45,11 +46,9 @@ done
 export PATH
 
 # PYTHONPATH=~/lib/python;		export PYTHONPATH
-PYTHONSTARTUP=~/.pystartup;		export PYTHONSTARTUP
-AWT_TOOLKIT=MToolkit;			export AWT_TOOLKIT
-_JAVA_AWT_WM_NONREPARENTING=1;	export _JAVA_AWT_WM_NONREPARENTING
-
-#[ -z "$GPG_AGENT_INFO" ] && [ -e $HOME/.gnupg/gpg-agent-info-$(hostname) ] && . $HOME/.gnupg/gpg-agent-info-$(hostname) && export GPG_AGENT_INFO
+export PYTHONSTARTUP=~/.pystartup
+export AWT_TOOLKIT=MToolkit
+export _JAVA_AWT_WM_NONREPARENTING=1
 
 # source personal settings from .zshother
 [ -e $HOME/.zshother ] && . ~/.zshother
