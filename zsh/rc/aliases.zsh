ac868e9c17e564279268057186bc9fdc921d3269
Jan Christoph Ebersbach <jceb@e-jc.de>
2015-06-11 08:14:45 +0200

    add more global aliases

--- zsh/rc/aliases.zsh
+++ zsh/rc/aliases.zsh
@@ -1,6 +1,8 @@
 # global aliases that can be appended to line
 alias -g L="|less"
+alias -g LL="|& less"
 alias -g G="|g"
+alias -g GG="|& g"
 
 alias grep='grep --color=auto --exclude=\*\.svn-base --exclude=\*\~ --exclude=\*\.tmp --binary-files=without-match'
 alias o='xdg-open'
