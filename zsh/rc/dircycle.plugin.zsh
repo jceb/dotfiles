1b4f4484c7d3a062f369a73ec1618ce8d8319c4d
Jan Christoph Ebersbach <jceb@e-jc.de>
2016-02-27 12:34:41 +0100

    Search three level deep for directories

--- zsh/rc/dircycle.plugin.zsh
+++ zsh/rc/dircycle.plugin.zsh
@@ -22,9 +22,9 @@ bindkey '^[u' cdUp
 if type fzf &> /dev/null; then
 	fzChDir() { cd "$(find -L . -mindepth 1 -type d -o -type l ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* | fzf)"; zle reset-prompt; }
 	zle -N fzChDir
-	bindkey '^f' fzChDir
+	bindkey '^[N' fzChDir
 
-	fzChDirOne() { cd "$(find -L . -mindepth 1 -maxdepth 2 -type d -o -type l ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* | fzf)"; zle reset-prompt; }
+	fzChDirOne() { cd "$(find -L . -mindepth 1 -maxdepth 3 -type d -o -type l ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* | fzf)"; zle reset-prompt; }
 	zle -N fzChDirOne
 	# M-n
 	bindkey '^[n' fzChDirOne
@@ -36,8 +36,7 @@ if type fzf &> /dev/null; then
 elif type qf &> /dev/null; then
 	qfChDir() { cd "$(qf -d -o)"; zle reset-prompt; }
 	zle -N qfChDir
-	bindkey -r '^f'
-	bindkey '^f' qfChDir
+	bindkey '^[n' qfChDir
 
 	qfEdit() { BUFFER='EDITOR=vserver qf'; zle accept-line }
 	zle -N qfEdit
