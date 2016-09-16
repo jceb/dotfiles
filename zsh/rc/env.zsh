bb4f02c0dfd6f7bb1fc499123dceaa68371b7dd6
Jan Christoph Ebersbach <ebersbach@univention.de>
2016-09-16 09:29:22 +0200

    Put variable values in parentheses

--- zsh/rc/env.zsh
+++ zsh/rc/env.zsh
@@ -1,56 +1,57 @@
-export HISTFILE=~/.historyzsh
-export HISTSIZE=1000
-export SAVEHIST=1000
-export DIRSTACKSIZE=11
+export HISTFILE="${HOME}/.historyzsh"
+export HISTSIZE="1000"
+export SAVEHIST="1000"
+export DIRSTACKSIZE="11"
+export TMPDIR="${HOME}/.cache/tmp"
 
 if $(locale -a|grep -q en_US.utf8); then
-	export LANG=en_US.UTF-8
+	export LANG="en_US.UTF-8"
 else
-	export LANG=C
+	export LANG="C"
 fi
 # to get around sorting issues, define differently
 export LC_COLLATE="C"
 
-export EDITOR=nvim
+export EDITOR="nvim"
 # export GIT_EDITOR=~/.local/bin/nvim
 # change cursor shape in nvim
-export NVIM_TUI_ENABLE_CURSOR_SHAPE=1
+export NVIM_TUI_ENABLE_CURSOR_SHAPE="1"
 # use true colors in the terminal - seems to produce weird colors
-# export NVIM_TUI_ENABLE_TRUE_COLOR=1
+# export NVIM_TUI_ENABLE_TRUE_COLOR="1"
 
 # export EMAIL=put me in ~/.zshother
-export BROWSER=x-www-browser
+export BROWSER="x-www-browser"
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
 # add -F to automatically quit less if page fits on one screen
 export LESS="--ignore-case -rSX"
-export PAGER=less
+export PAGER="less"
 
 # export TTY properly to support gnupg
-export GPG_TTY=$(tty)
+export GPG_TTY="$(tty)"
 if [ -z "${GPG_AGENT_INFO}" ]; then
-	export GPG_AGENT_INFO="$HOME/.gnupg/S.gpg-agent"
+	export GPG_AGENT_INFO="${HOME}/.gnupg/S.gpg-agent"
 fi
 
 # quilt settings, always look for patches in the debian/patches directory
-export QUILT_PATCHES=debian/patches
+export QUILT_PATCHES="debian/patches"
 export QUILT_DIFF_ARGS="--color=auto"
 
 # bc settings
-export BC_ENV_ARGS=~/.bcrc
+export BC_ENV_ARGS="${HOME}/.bcrc"
 
 # go lang settings
 export GOPATH="${HOME}/.local/go"
 
 # set PATH so it includes user's private bin if it exists
-for i in ${HOME}/.gem/ruby/*/bin "${HOME}/Documents/toolshed/" "${HOME}/.cabal/bin" "${HOME}/.local/bin" "${HOME}/bin" "${HOME}/.gem/ruby/2.3.0/bin" "${GOPATH}/bin" "${HOME}/node_modules/ttystudio/bin"; do
+for i in "${HOME}/.gem/ruby/*/bin" "${HOME}/Documents/toolshed/" "${HOME}/.cabal/bin" "${HOME}/.local/bin" "${HOME}/bin" "${HOME}/.gem/ruby/2.3.0/bin" "${GOPATH}/bin" "${HOME}/node_modules/ttystudio/bin"; do
 	if [ -e "$i" ]; then
 		PATH="$i:${PATH}"
 	fi
 done
 export PATH
-for i in ${HOME}/.local/share/man "/"; do
+for i in "${HOME}/.local/share/man" "/"; do
 	if [ -e "$i" ]; then
 		MANPATH="$i:${MANPATH}"
 	fi
@@ -58,9 +59,9 @@ done
 export MANPATH
 
 # PYTHONPATH=~/lib/python;		export PYTHONPATH
-export PYTHONSTARTUP=~/.pystartup
-export AWT_TOOLKIT=MToolkit
-export _JAVA_AWT_WM_NONREPARENTING=1
+export PYTHONSTARTUP="${HOME}/.pystartup"
+export AWT_TOOLKIT="MToolkit"
+export _JAVA_AWT_WM_NONREPARENTING="1"
 
 # source personal settings from .zshother
 [ -e $HOME/.zshother ] && . ~/.zshother
