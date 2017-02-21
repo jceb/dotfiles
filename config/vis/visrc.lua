e1cff0f41ee570a37450ea19ad22f86095e053a3
Jan Christoph Ebersbach <jceb@e-jc.de>
2017-02-21 06:55:56 +0100

    Update visrc

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -1,2 +1,17 @@
-vis:command('set theme papercolor')
-vis:command('set theme autoindent')
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+require('plugins/filetype')
+require('plugins/textobject-lexer')
+
+vis.events.subscribe(vis.events.INIT, function()
+	vis:command('set theme papercolor')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	vis:command('set number')
+	vis:command('set relativenumbers')
+	vis:command('set autoindent')
+	vis:command('set colorcolumn 80')
+	vis:command('set show-tabs')
+end)
+
