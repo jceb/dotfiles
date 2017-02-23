6740178712ca1122052acd18a162b3f873dea0b1
Jan Christoph Ebersbach <jceb@e-jc.de>
2017-02-23 08:49:31 +0100

    Convert to inline fzf completion

--- zsh/rc/dircycle.plugin.zsh
+++ zsh/rc/dircycle.plugin.zsh
@@ -20,44 +20,70 @@ bindkey '^[u' cdUp
 # bindkey 'õ' cdUp
 
 if type fzf &> /dev/null; then
-    fzChDir() { cd "$(find -L . -mindepth 1 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* | fzf)"; zle reset-prompt; }
+	fzChDir() {
+		setopt localoptions pipefail 2> /dev/null
+		local dir="$(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%}" FZF_DEFAULT_COMMAND="find -L . -mindepth 1 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\*" $(__fzfcmd))"
+		local ret=$?
+		if [ -d "${dir}" ]; then
+			cd "${dir}"
+		else
+			ret=1
+		fi
+		zle reset-prompt
+		typeset -f zle-line-init >/dev/null && zle zle-line-init
+		return $ret
+	}
 	zle -N fzChDir
 	bindkey '^[N' fzChDir
 
-    fzChDirOne() { cd "$(find -L . -mindepth 1 -maxdepth 3 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* | fzf)"; zle reset-prompt; }
+	fzChDirOne() {
+		setopt localoptions pipefail 2> /dev/null
+		local dir="$(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" FZF_DEFAULT_COMMAND="find -L . -mindepth 1 -maxdepth 3 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\*" $(__fzfcmd))"
+		local ret=$?
+		if [ -d "${dir}" ]; then
+			cd "${dir}"
+		else
+			ret=1
+		fi
+		zle reset-prompt
+		typeset -f zle-line-init >/dev/null && zle zle-line-init
+		return $ret
+	}
 	zle -N fzChDirOne
 	# M-n
 	bindkey '^[n' fzChDirOne
 	# bindkey 'î' fzChDirOne
 
-	fzEdit() { BUFFER='FILES=($(fzf -m)) && "${EDITOR}" "${FILES[@]}"'; zle accept-line }
+	fzEdit() { BUFFER='FILES=($(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse -m" $(__fzfcmd))) && "${EDITOR}" "${FILES[@]}"'; zle accept-line }
 	zle -N fzEdit
 	bindkey '^\' fzEdit
 
-	fzOpen() { BUFFER='FILES=($(fzf -m)) && xdg-open "${FILES[@]}"'; zle accept-line }
+	fzOpen() { BUFFER='FILES=($(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse -m" $(__fzfcmd))) && xdg-open "${FILES[@]}"'; zle accept-line }
 	zle -N fzOpen
 	bindkey "^]" fzOpen
-elif type qf &> /dev/null; then
-	qfChDir() { cd "$(qf -d -o)"; zle reset-prompt; }
-	zle -N qfChDir
-	bindkey '^[n' qfChDir
 
-	qfEdit() { BUFFER='qf'; zle accept-line }
-	zle -N qfEdit
-	bindkey '^\' qfEdit
+	warpDir() {
+		local FILE=
+		local ret=1
+		if test -e "$HOME/.warprc"; then
+			FILE="$HOME/.warprc"
+		elif test -e "$HOME/.cdargs"; then
+			FILE="$HOME/.cdargs"
+		fi
+		if [ -n "${FILE}" ]; then
+			setopt localoptions pipefail 2> /dev/null
+			local dir="$(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" FZF_DEFAULT_COMMAND="sed -e "s/:/\\\\t/" < '${FILE}'" $(__fzfcmd) | sed -ne "s/^[^\t]*\t//p")"
+			local ret=$?
+			if [ -d "${dir}" ]; then
+				cd "${dir}"
+			else
+				ret=1
+			fi
+		fi
+		zle reset-prompt
+		typeset -f zle-line-init >/dev/null && zle zle-line-init
+		return $ret
+	}
+	zle -N warpDir
+	bindkey '^b' warpDir
 fi
-
-warpDir() {
-	FILE=
-	if test -e ~/.warprc; then
-		FILE=~/.warprc
-	elif test -e ~/.cdargs; then
-		FILE=~/.cdargs
-	fi
-	if [ -n "${FILE}" ]; then
-		cd "$(sed -e "s/:/\t/" < ${FILE} | fzf | sed -ne "s/^[^\t]*\t//p")"
-	fi
-	zle reset-prompt
-}
-zle -N warpDir
-bindkey '^b' warpDir
