d9fbd3e7302ee0afba58878b624d7d00c0df1f0a
Jan Christoph Ebersbach <jceb@e-jc.de>
2017-03-23 07:08:28 +0100

    Don't use FZF_DEFAULT_COMMAND anymore

--- zsh/rc/dircycle.plugin.zsh
+++ zsh/rc/dircycle.plugin.zsh
@@ -22,7 +22,7 @@ bindkey '^[u' cdUp
 if type fzf &> /dev/null; then
 	fzChDir() {
 		setopt localoptions pipefail 2> /dev/null
-		local dir="$(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%}" FZF_DEFAULT_COMMAND="find -L . -mindepth 1 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\*" $(__fzfcmd))"
+		local dir="$(find -L . -mindepth 1 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* 2>/dev/null | FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" $(__fzfcmd))"
 		local ret=$?
 		if [ -d "${dir}" ]; then
 			cd "${dir}"
@@ -38,7 +38,7 @@ if type fzf &> /dev/null; then
 
 	fzChDirOne() {
 		setopt localoptions pipefail 2> /dev/null
-		local dir="$(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" FZF_DEFAULT_COMMAND="find -L . -mindepth 1 -maxdepth 3 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\*" $(__fzfcmd))"
+		local dir="$(find -L . -mindepth 1 -maxdepth 3 \( -type d -o -type l \) ! -wholename \*/debian/\*/\* ! -wholename \*/.svn/\* ! -wholename \*/.git/modules/\* ! -wholename \*/.git/objects/\* ! -wholename \*/.hg/\* 2>/dev/null | FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" $(__fzfcmd))"
 		local ret=$?
 		if [ -d "${dir}" ]; then
 			cd "${dir}"
@@ -54,11 +54,25 @@ if type fzf &> /dev/null; then
 	bindkey '^[n' fzChDirOne
 	# bindkey 'î' fzChDirOne
 
-	fzEdit() { BUFFER='FILES=($(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse -m" $(__fzfcmd))) && "${EDITOR}" "${FILES[@]}"'; zle accept-line }
+	fzEdit() {
+		FILES="$(rg -i --files --no-ignore --hidden --follow --glob "!.git/*" 2>/dev/null | FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse -m" $(__fzfcmd))"
+		if [ -n "${FILES}" ]; then
+			BUFFER='"${EDITOR}" "${FILES[@]}"'
+			zle accept-line
+			zle reset-prompt
+		fi
+	}
 	zle -N fzEdit
 	bindkey '^\' fzEdit
 
-	fzOpen() { BUFFER='FILES=($(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse -m" $(__fzfcmd))) && xdg-open "${FILES[@]}"'; zle accept-line }
+	fzOpen() {
+		FILES="$(rg -i --files --no-ignore --hidden --follow --glob "!.git/*" 2>/dev/null | FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse -m" $(__fzfcmd))"
+		if [ -n "${FILES}" ]; then
+			BUFFER='xdg-open "${FILES[@]}"'
+			zle accept-line
+			zle reset-prompt
+		fi
+	}
 	zle -N fzOpen
 	bindkey "^]" fzOpen
 
@@ -72,7 +86,7 @@ if type fzf &> /dev/null; then
 		fi
 		if [ -n "${FILE}" ]; then
 			setopt localoptions pipefail 2> /dev/null
-			local dir="$(FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" FZF_DEFAULT_COMMAND="sed -e "s/:/\\\\t/" < '${FILE}'" $(__fzfcmd) | sed -ne "s/^[^\t]*\t//p")"
+			local dir="$(sed -e "s/:/\\t/" < "${FILE}" | FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height ${FZF_TMUX_HEIGHT:-40%} --reverse" $(__fzfcmd) | sed -ne "s/^[^\t]*\t//p")"
 			local ret=$?
 			if [ -d "${dir}" ]; then
 				cd "${dir}"
