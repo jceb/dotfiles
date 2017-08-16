afc052bce1532f3bf96b26e4bc5b04a5452e4052
Jan Christoph Ebersbach <jceb@e-jc.de>
2017-08-16 19:42:52 +0200

    Update snippets

--- config/autokey/data/My Phrases/MAC.py
+++ config/autokey/data/My Phrases/MAC.py
@@ -3,6 +3,6 @@
 import time
 import random
 t = str(int(time.time()))[-10:]
-set_unicast_mac = 254 # least significant bit 0 
+set_unicast_mac = (1 << 8) - (1 << 1) # least significant bit 0 
 set_local_administrated_mac = 1 << 1 # second least significant bit 1
 keyboard.send_keys(':'.join([hex(random.randint(0, 255) & set_unicast_mac | set_local_administrated_mac)[2:].rjust(2, '0')] + [ j + k for j, k in zip(t[::2], t[1::2])]))
