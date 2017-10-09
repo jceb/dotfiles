8d4dbce388597014f88c6d5d1973a237fc3dc071
Jan Christoph Ebersbach <jceb@e-jc.de>
2017-10-09 18:03:31 +0900

    Show message when no patches are applied

--- config/fish/functions/__fish_quilt_prompt.fish
+++ config/fish/functions/__fish_quilt_prompt.fish
@@ -13,7 +13,7 @@ function __fish_quilt_prompt -d "Write out the quilt prompt"
     if not command -sq quilt
         return 1
     end
-    if not command quilt top > /dev/null ^ /dev/null
+    if not command quilt series > /dev/null ^ /dev/null
         return 1
     end
     set -l patches
@@ -22,12 +22,18 @@ function __fish_quilt_prompt -d "Write out the quilt prompt"
     end
     set -l npatches (count $patches)
     set -l current_patch (command quilt top)
-    set -l idx_patch (contains -i $current_patch $patches)
+    set -l idx_patch "0"
+    if test -n "$current_patch"
+        set idx_patch (contains -i $current_patch $patches)
+        set current_patch (basename $current_patch)
+    else
+        set current_patch "No patch applied"
+    end
     __fish_quilt_set_color __fish_quilt_prompt_color (set_color blue)
     set -l format $argv[1]
     if test -z "$format"
         set format " (%s)"
     end
-    set prompt "$__fish_quilt_prompt_color"(basename $current_patch)" $idx_patch/$npatches$__fish_quilt_prompt_color_done"
+    set prompt "$__fish_quilt_prompt_color$current_patch $idx_patch/$npatches$__fish_quilt_prompt_color_done"
     printf $format $prompt
 end
