de00c2fa661079d7462a7e12a6bf13e757b46fe0
Jan Christoph Ebersbach <jceb@e-jc.de>
2018-02-10 21:00:05 +0100

    Update fzf fish

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -21,18 +21,14 @@ function fish_user_key_bindings
     bind -M insert \en __fish_dir_cycle_fzChDir
 
     ### fzf ###
-    set -q FZF_LEGACY_KEYBINDINGS
-    or set -l FZF_LEGACY_KEYBINDINGS 1
     if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
         bind \ct '__fzf_find_file'
         bind \cr '__fzf_reverse_isearch'
-        bind \cx '__fzf_find_and_execute'
         bind \ec '__fzf_cd'
         bind \eC '__fzf_cd_with_hidden'
         if bind -M insert >/dev/null ^/dev/null
             bind -M insert \ct '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \cx '__fzf_find_and_execute'
             bind -M insert \ec '__fzf_cd'
             bind -M insert \eC '__fzf_cd_with_hidden'
         end
@@ -41,13 +37,12 @@ function fish_user_key_bindings
         bind \cr '__fzf_reverse_isearch'
         bind \ex '__fzf_find_and_execute'
         bind \eo '__fzf_cd'
-        bind \eO '__fzf_cd_with_hidden'
+        bind \eO '__fzf_cd --hidden'
         if bind -M insert >/dev/null ^/dev/null
             bind -M insert \cf '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \ex '__fzf_find_and_execute'
             bind -M insert \eo '__fzf_cd'
-            bind -M insert \eO '__fzf_cd_with_hidden'
+            bind -M insert \eO '__fzf_cd --hidden'
         end
     end
     ### fzf ###
