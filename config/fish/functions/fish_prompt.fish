c42ddd2602f5a29fdf3724c745246b25948295d5
Jan Christoph Ebersbach <jceb@e-jc.de>
2018-02-22 14:12:12 +0100

    Fixes redraw issues when changing the directory with a keybinding

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -83,6 +83,8 @@ function fish_prompt --description 'Write out the prompt'
         else if  test -n "$quilt_prompt"
             set vcs_prompt " ($quilt_prompt)"
         end
+        # Fixes redraw issues when changing the directory with a keybinding https://github.com/fish-shell/fish-shell/issues/717
+        echo -ne '\033[K'
         if test "$stat" -ne 0
             printf '%s(%s) %s%s%s%s%s\n%s%s%s@%s %% ' "$__fish_color_status" "$stat" "$__fish_prompt_normal" "$__fish_prompt_cwd" "$pwd" "$__fish_color_magenta" "$vcs_prompt" "$__fish_color_blue" "$USER" "$__fish_prompt_normal" (prompt_hostname)
         else
