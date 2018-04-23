5dd9724aa4fbcfdd114f0f443a054a4829c8eea3
Jan Christoph Ebersbach <jceb@e-jc.de>
2018-04-23 09:03:39 +0200

    Update bindings

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -26,7 +26,7 @@ function fish_user_key_bindings
         bind \cr '__fzf_reverse_isearch'
         bind \ec '__fzf_cd'
         bind \eC '__fzf_cd_with_hidden'
-        if bind -M insert >/dev/null ^/dev/null
+        if bind -M insert >/dev/null 2>/dev/null
             bind -M insert \ct '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
             bind -M insert \ec '__fzf_cd'
@@ -37,14 +37,15 @@ function fish_user_key_bindings
         bind \cr '__fzf_reverse_isearch'
         bind \eo '__fzf_cd'
         bind \eO '__fzf_cd --hidden'
-        if bind -M insert >/dev/null ^/dev/null
+        if bind -M insert >/dev/null 2>/dev/null
             bind -M insert \cf '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
             bind -M insert \eo '__fzf_cd'
             bind -M insert \eO '__fzf_cd --hidden'
         end
     end
-    set -q FZF_COMPLETE
-    and bind \t '__fzf_complete'
+    if set -q FZF_COMPLETE
+        bind \t '__fzf_complete'
+    end
     ### fzf ###
 end
