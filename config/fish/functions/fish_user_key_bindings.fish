ffc44bef864520a3fb29fd2017becb145c415f35
Jan Christoph Ebersbach <jceb@e-jc.de>
2018-06-16 22:12:14 +0200

    Change bookmark binding to c-k

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -16,7 +16,7 @@ function fish_user_key_bindings
     bind -M insert \ed __fish_list_current_token
     bind -M insert \em __fish_man_page
 
-    bind -M insert \cb __fish_dir_cycle_warpDir
+    bind -M insert \ck __fish_dir_cycle_warpDir
 
     bind -M insert \en __fish_dir_cycle_fzChDir
 
