e9543627c2fc959be75f666e132af06ffa0d4ee2
Jan Christoph Ebersbach <jceb@e-jc.de>
2019-01-29 07:51:54 +0100

    Update aliases to fish 3

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -3,7 +3,7 @@ function issh
     command ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "PreferredAuthentications=keyboard-interactive" $argv
 end
 
-abbr --add dockeri="docker run --rm -i -t"
+abbr --add dockeri 'docker run --rm -i -t'
 
 # defaults
 function grep
@@ -18,27 +18,27 @@ function any
     command ps aux | rg $argv
 end
 
-abbr --add 'o=xdg-open'
-abbr --add 'open=xdg-open'
+abbr --add o 'xdg-open'
+abbr --add open 'xdg-open'
 
 # on debian based systems this makes the use of ack a bit easier
 if test -e /usr/bin/ack-grep
-    abbr --add ack=ack-grep
+    abbr --add ack 'ack-grep'
 end
 
-abbr --add 't=tree -f'
+abbr --add t 'tree -f'
 
 # calendar and contacts abbreviations
-abbr --add 'cal=khal'
-abbr --add 'agenda=khal agenda'
-abbr --add 'c=khard'
+abbr --add cal 'khal'
+abbr --add agenda 'khal agenda'
+abbr --add c 'khard'
 
 # ls
 function ls
     command ls -b -CF --file-type --color=auto --group-directories-first $argv
 end
-abbr --add 'ltr=ls -ltr'
-abbr --add 'ltra=ls -ltra'
+abbr --add ltr 'ls -ltr'
+abbr --add ltra 'ls -ltra'
 function l
     ls -l $argv
 end
@@ -50,57 +50,57 @@ function lh
 end
 
 # quilt
-abbr --add 'q++=quilt push -a'
-abbr --add 'q+=quilt push'
-abbr --add 'q--=quilt pop -a'
-abbr --add 'q-=quilt pop'
-abbr --add 'q=quilt'
-abbr --add 'qD=quilt delete -r'
-abbr --add 'qa=quilt add'
-abbr --add 'qd=quilt diff'
-abbr --add 'qe=quilt new'
-abbr --add 'qp=quilt pop'
-abbr --add 'qr=quilt refresh'
-abbr --add 'qs=quilt series'
-abbr --add 'qt=quilt top'
+abbr --add q++ 'quilt push -a'
+abbr --add q+ 'quilt push'
+abbr --add q-- 'quilt pop -a'
+abbr --add q- 'quilt pop'
+abbr --add q 'quilt'
+abbr --add qD 'quilt delete -r'
+abbr --add qa 'quilt add'
+abbr --add qd 'quilt diff'
+abbr --add qe 'quilt new'
+abbr --add qp 'quilt pop'
+abbr --add qr 'quilt refresh'
+abbr --add qs 'quilt series'
+abbr --add qt 'quilt top'
 
 # git
-abbr --add 'g+=git stash pop'
-abbr --add 'g-=git stash'
-abbr --add 'g=git'
-abbr --add 'ga=git add'
-abbr --add 'gau=git add -u'
-abbr --add 'gb=git branch'
-abbr --add 'gba=git ba'
-abbr --add 'gbam=git bam'
-abbr --add 'gbc=git bc'
-abbr --add 'gbm=git bm'
-abbr --add 'gbr=git bc'
-abbr --add 'gc=git commit'
-abbr --add 'gca=git commit -a'
-abbr --add 'gci=git ci'
-abbr --add 'gcmsg=git commit --amend'
-abbr --add 'gco=git checkout'
-abbr --add 'gd=git diff --no-prefix'
-abbr --add 'gdc=git diffc'
-abbr --add 'gdd=git difff'
-abbr --add 'gdw=git diff --no-prefix --word-diff'
-abbr --add 'ge=git edit'
-abbr --add 'gf=git fetch; and git log ...(git rev-parse --abbrev-ref @\{upstream\})'
-abbr --add 'gl=git log'
-abbr --add 'glu=git log ..(git rev-parse --abbrev-ref @\{upstream\})'
-abbr --add 'gm=git merge'
-abbr --add 'gp=git push'
-abbr --add 'gpre=git pre'
-abbr --add 'grm=git rm'
-abbr --add 'gst=git st'
-abbr --add 'gsta=git sta'
-abbr --add 'gsuba=git submodule add'
-abbr --add 'gsubm=git subm'
-abbr --add 'gsubpre=git subpre'
-abbr --add 'gsubup=git subup'
-abbr --add 'guc=git commit -m "Update changelogs"'
-abbr --add 'gup=git up'
+abbr --add g+ 'git stash pop'
+abbr --add g- 'git stash'
+abbr --add g 'git'
+abbr --add ga 'git add'
+abbr --add gau 'git add -u'
+abbr --add gb 'git branch'
+abbr --add gba 'git ba'
+abbr --add gbam 'git bam'
+abbr --add gbc 'git bc'
+abbr --add gbm 'git bm'
+abbr --add gbr 'git bc'
+abbr --add gc 'git commit'
+abbr --add gca 'git commit -a'
+abbr --add gci 'git ci'
+abbr --add gcmsg 'git commit --amend'
+abbr --add gco 'git checkout'
+abbr --add gd 'git diff --no-prefix'
+abbr --add gdc 'git diffc'
+abbr --add gdd 'git difff'
+abbr --add gdw 'git diff --no-prefix --word-diff'
+abbr --add ge 'git edit'
+abbr --add gf 'git fetch; and git log ...(git rev-parse --abbrev-ref @\{upstream\})'
+abbr --add gl 'git log'
+abbr --add glu 'git log ..(git rev-parse --abbrev-ref @\{upstream\})'
+abbr --add gm 'git merge'
+abbr --add gp 'git push'
+abbr --add gpre 'git pre'
+abbr --add grm 'git rm'
+abbr --add gst 'git st'
+abbr --add gsta 'git sta'
+abbr --add gsuba 'git submodule add'
+abbr --add gsubm 'git subm'
+abbr --add gsubpre 'git subpre'
+abbr --add gsubup 'git subup'
+abbr --add guc 'git commit -m "Update changelogs"'
+abbr --add gup 'git up'
 
 function _ch2root --description "jump to the next parent directory containing a subdirectory that's passed via argv"
     set _d $PWD
@@ -118,9 +118,9 @@ function _ch2root --description "jump to the next parent directory containing a 
     return
 end
 
-abbr --add 'cd.=cd (_ch2root debian .git .hg .svn)'
+abbr --add cd. 'cd (_ch2root debian .git .hg .svn)'
 
-abbr --add 'cd..=cd ..'
+abbr --add cd.. 'cd ..'
 
 function r -d 'grep replacement'
     rg -S --hidden -n -H $argv | fzf -0 -m
