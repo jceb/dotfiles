ac2db3e4d7056893b85b0d6cc2cc140248d38b06
Jan Christoph Ebersbach <jan-christoph.ebersbach@chainstep.com>
2019-10-15 21:41:55 +0200

    Hide user name if it's the same as the currently active user

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -87,12 +87,24 @@ function fish_prompt --description 'Write out the prompt'
             set vcs_prompt " ($quilt_prompt)"
         end
 
+        set user ''
+        if test "$USER" != (whoami)
+            set user (whoami)
+        end
+
         # Fixes redraw issues when changing the directory with a keybinding https://github.com/fish-shell/fish-shell/issues/717
         echo -ne '\033[K'
         if test "$stat" -ne 0
-            printf '%s(%s) %s%s%s%s%s\n%s%s%s@%s %% ' "$__fish_color_status" "$stat" "$__fish_prompt_normal" "$__fish_prompt_cwd" (prompt_pwd) "$__fish_color_magenta" "$vcs_prompt" "$__fish_color_blue" "$USER" "$__fish_prompt_normal" (prompt_hostname)
+            printf '%s(%s) %s%s%s@%s %s%s%s%s\n%s➤ ' \
+            "$__fish_color_status" "$stat" \
+            "$__fish_color_blue" "$user" "$__fish_prompt_normal"  (prompt_hostname) \
+            "$__fish_prompt_cwd" (prompt_pwd) "$__fish_color_magenta" "$vcs_prompt" \
+            "$__fish_color_yellow➤"
         else
-            printf '%s%s%s%s%s\n%s%s%s@%s %% ' "$__fish_prompt_normal" "$__fish_prompt_cwd" (prompt_pwd) "$__fish_color_magenta" "$vcs_prompt" "$__fish_color_blue" "$USER" "$__fish_prompt_normal" (prompt_hostname)
+            printf '%s%s%s@%s %s%s%s%s\n%s➤ ' \
+            "$__fish_color_blue" "$user" "$__fish_prompt_normal"  (prompt_hostname) \
+            "$__fish_prompt_cwd" (prompt_pwd) "$__fish_color_magenta" "$vcs_prompt" \
+            "$__fish_color_yellow"
         end
     end
 end
