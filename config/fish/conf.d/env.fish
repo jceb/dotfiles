929cd3a7f431f1dbc2fd77ce67cac0e5e50d7639
Jan Christoph Ebersbach <jan-christoph.ebersbach@chainstep.com>
2020-05-08 11:14:26 +0200

    Move environment variables into systemd's care

--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -1,77 +1,81 @@
-set -x TMPDIR "$HOME/.cache/tmp"
-# speed up certain system calls: https://blog.packagecloud.io/eng/2017/02/21/set-environment-variable-save-thousands-of-system-calls/
-# WARNING this advice seems to break Thunderbird's time display
-# set -x TZ :/etc/localtime
+# export TMPDIR="$HOME/.cache/tmp"
+# # speed up certain system calls: https://blog.packagecloud.io/eng/2017/02/21/set-environment-variable-save-thousands-of-system-calls/
+# # WARNING this advice seems to break Thunderbird's time display
+# # export TZ=:/etc/localtime
+#
+# if locale -a|grep -q en_US.utf8
+# 	export LANG="en_US.UTF-8"
+# else
+# 	export LANG="C"
+# end
+# # to get around sorting issues, define differently
+# export LC_COLLATE="en_US.UTF-8"
+# # export LC_COLLATE="C"
+#
+# export EDITOR="vim"
+# # export GIT_EDITOR ~/.local/bin/nvim
+# # use true colors in the terminal - seems to produce weird colors
+# export NVIM_TUI_ENABLE_TRUE_COLOR="1"
+#
+# # export EMAIL put me in ~/.config/fish/conf.d/other.fish
+# # export EMAIL "jceb@e-jc.de"
+# export BROWSER="x-www-browser"
+#
+# # make less more friendly for non-text input files, see lesspipe(1)
+# if test -x /usr/bin/lesspipe
+# 	eval (lesspipe)
+# end
+# # add -F to automatically quit less if page fits on one screen
+# export LESS="--ignore-case -rSX"
+# export PAGER="less"
 
-if locale -a|grep -q en_US.utf8
-	set -x LANG "en_US.UTF-8"
-else
-	set -x LANG "C"
-end
-# to get around sorting issues, define differently
-set -x LC_COLLATE "en_US.UTF-8"
-# set -x LC_COLLATE "C"
+# export TTY properly to support gnupg
+export GPG_TTY=(tty)
 
-set -x EDITOR "vim"
-# set -x GIT_EDITOR ~/.local/bin/nvim
-# use true colors in the terminal - seems to produce weird colors
-set -x NVIM_TUI_ENABLE_TRUE_COLOR "1"
+# if test -z "$GPG_AGENT_INFO"
+#     export GPG_AGENT_INFO="/run/user/$(getent passwd jceb|awk -F: '{print $3}')/gnupg/S.gpg-agent"
+# end
+# if test -z "$SSH_AUTH_SOCK"
+#     export SSH_AUTH_SOCK="/run/user/$(getent passwd jceb|awk -F: '{print $3}')/gnupg/S.gpg-agent.ssh"
+# end
 
-# set -x EMAIL put me in ~/.config/fish/conf.d/other.fish
-# set -x EMAIL "jceb@e-jc.de"
-set -x BROWSER "x-www-browser"
-
-# make less more friendly for non-text input files, see lesspipe(1)
-if test -x /usr/bin/lesspipe
-	eval (lesspipe)
-end
-# add -F to automatically quit less if page fits on one screen
-set -x LESS "--ignore-case -rSX"
-set -x PAGER "less"
-
-# set -x TTY properly to support gnupg
-set -x GPG_TTY (tty)
-if test -z "$GPG_AGENT_INFO"
-	set -x GPG_AGENT_INFO "$HOME/.gnupg/S.gpg-agent"
-end
-
-set -x FZF_DEFAULT_COMMAND 'command rg -i --files --no-ignore --hidden --follow --glob "!.git/*"'
-set -x FZF_DEFAULT_OPTS '--bind ctrl-z:toggle-all --color=light'
-
-# quilt settings, always look for patches in the debian/patches directory
-set -x QUILT_PATCHES "debian/patches"
-set -x QUILT_DIFF_ARGS "--color=auto"
-
-# bc settings
-set -x BC_ENV_ARGS "$HOME/.bcrc"
-
-# go lang settings
-set -x GOPATH "$HOME/.local/go"
-
-set -x ANDROID_HOME "$HOME/Android/Sdk"
-
-set -x LD_LIBRARY_PATH "$HOME/.local/lib"
-
-# set PATH so it includes user's private bin if it exists
-for i in "$HOME/Documents/toolshed/" "$HOME/.cabal/bin" "$HOME/.local/bin" "$HOME/bin" "$HOME/.gem/ruby/2.3.0/bin" "$HOME/.gem/ruby/2.4.0/bin" "$GOPATH/bin" "$HOME/.yarn/bin" "$HOME/.npm-global/bin" "$HOME/.cargo/bin" "$ANDROID_HOME/emulator" "$ANDROID_HOME/platform-tools" "$ANDROID_HOME/tools" "/var/lib/snapd/snap/bin"
-	if test -d "$i"; and not contains $i $PATH
-		set -x PATH $i $PATH
-	end
-end
-
-for i in "$HOME/.local/share/man" '/usr/share/man'
-	if test -d "$i"; and not contains $i $MANPATH
-		set -x MANPATH "$i:$MANPATH"
-	end
-end
-
-# PYTHONPATH=~/lib/python;		set -x PYTHONPATH
-set -x PYTHONSTARTUP "$HOME/.pystartup"
-
-set -x XTERM_VERSION 'XTerm(327)'
-
-set -x HIGHLIGHT_OPTIONS '--style=seashell'
-
-set -x WINIT_HIDPI_FACTOR '1.0'
-
-# vi: ft=fish:tw=0:sw=4:ts=4
+# export FZF_DEFAULT_COMMAND='command rg -i --files --no-ignore --hidden --follow --glob "!.git/*"'
+# export FZF_DEFAULT_OPTS='--bind ctrl-z:toggle-all --color=light'
+#
+# # quilt settings, always look for patches in the debian/patches directory
+# export QUILT_PATCHES="debian/patches"
+# export QUILT_DIFF_ARGS="--color=auto"
+#
+# # bc settings
+# export BC_ENV_ARGS="$HOME/.bcrc"
+#
+# # go lang settings
+# export GOPATH="$HOME/.local/go"
+#
+# export ANDROID_HOME="$HOME/Android/Sdk"
+#
+# export LD_LIBRARY_PATH="$HOME/.local/lib"
+#
+# # set PATH so it includes user's private bin if it exists
+# for i in "$HOME/Documents/toolshed/" "$HOME/.cabal/bin" "$HOME/.local/bin" "$HOME/bin" "$HOME/.gem/ruby/2.3.0/bin" "$HOME/.gem/ruby/2.4.0/bin" "$GOPATH/bin" "$HOME/.yarn/bin" "$HOME/.npm-global/bin" "$HOME/.cargo/bin" "$ANDROID_HOME/emulator" "$ANDROID_HOME/platform-tools" "$ANDROID_HOME/tools" "/var/lib/snapd/snap/bin"
+# 	if test -d "$i"; and not contains $i $PATH
+# 		set -x PATH $i $PATH
+# 	end
+# end
+#
+# for i in "$HOME/.local/share/man" '/usr/share/man'
+# 	if test -d "$i"; and not contains $i $MANPATH
+# 		export MANPATH="$i:$MANPATH"
+# 	end
+# end
+#
+# # PYTHONPATH=~/lib/python;		export PYTHONPATH
+# export PYTHONSTARTUP="$HOME/.pystartup"
+#
+# export XTERM_VERSION='XTerm(327)'
+#
+# export HIGHLIGHT_OPTIONS='--style=seashell'
+#
+# export WINIT_HIDPI_FACTOR='1.0'
+#
+# # vi: ft=fish:tw=0:sw=4:ts=4
--- zsh/rc/env.zsh
+++ zsh/rc/env.zsh
@@ -1,83 +1,83 @@
-export HISTFILE="${HOME}/.historyzsh"
-export HISTSIZE="1000"
-export SAVEHIST="1000"
-export DIRSTACKSIZE="11"
-export TMPDIR="${HOME}/.cache/tmp"
-# speed up certain system calls: https://blog.packagecloud.io/eng/2017/02/21/set-environment-variable-save-thousands-of-system-calls/
-# WARNING this advice seems to break Thunderbird's time display
-# export TZ=:/etc/localtime
-
-if $(locale -a|grep -q en_US.utf8); then
-	export LANG="en_US.UTF-8"
-else
-	export LANG="C"
-fi
-# to get around sorting issues, define differently
-export LC_COLLATE="en_US.UTF-8"
-# export LC_COLLATE="C"
-
-export EDITOR="vim"
-# export GIT_EDITOR=~/.local/bin/nvim
-# use true colors in the terminal - seems to produce weird colors
-export NVIM_TUI_ENABLE_TRUE_COLOR="1"
-
-# export EMAIL=put me in ~/.zshother
-export BROWSER="x-www-browser"
-
-# make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
-# add -F to automatically quit less if page fits on one screen
-export LESS="--ignore-case -rSX"
-export PAGER="less"
+# export HISTFILE="${HOME}/.historyzsh"
+# export HISTSIZE="1000"
+# export SAVEHIST="1000"
+# export DIRSTACKSIZE="11"
+# export TMPDIR="${HOME}/.cache/tmp"
+# # speed up certain system calls: https://blog.packagecloud.io/eng/2017/02/21/set-environment-variable-save-thousands-of-system-calls/
+# # WARNING this advice seems to break Thunderbird's time display
+# # export TZ=:/etc/localtime
+#
+# if $(locale -a|grep -q en_US.utf8); then
+# 	export LANG="en_US.UTF-8"
+# else
+# 	export LANG="C"
+# fi
+# # to get around sorting issues, define differently
+# export LC_COLLATE="en_US.UTF-8"
+# # export LC_COLLATE="C"
+#
+# export EDITOR="vim"
+# # export GIT_EDITOR=~/.local/bin/nvim
+# # use true colors in the terminal - seems to produce weird colors
+# export NVIM_TUI_ENABLE_TRUE_COLOR="1"
+#
+# # export EMAIL=put me in ~/.zshother
+# export BROWSER="x-www-browser"
+#
+# # make less more friendly for non-text input files, see lesspipe(1)
+# [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+# # add -F to automatically quit less if page fits on one screen
+# export LESS="--ignore-case -rSX"
+# export PAGER="less"
 
 # export TTY properly to support gnupg
 export GPG_TTY="$(tty)"
-if [ -z "${GPG_AGENT_INFO}" ]; then
-	export GPG_AGENT_INFO="${HOME}/.gnupg/S.gpg-agent"
-fi
 
-export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --hidden --follow --glob "!.git/*"'
-export FZF_DEFAULT_OPTS='--bind ctrl-z:toggle-all --color=light'
-
-# quilt settings, always look for patches in the debian/patches directory
-export QUILT_PATCHES="debian/patches"
-export QUILT_DIFF_ARGS="--color=auto"
-
-# bc settings
-export BC_ENV_ARGS="${HOME}/.bcrc"
-
-# go lang settings
-export GOPATH="${HOME}/.local/go"
-
-export ANDROID_HOME="${HOME}/Android/Sdk"
-
-export LD_LIBRARY_PATH="${HOME}/.local/lib"
-
-# set PATH so it includes user's private bin if it exists
-for i in "$HOME/Documents/toolshed/" "$HOME/.cabal/bin" "$HOME/.local/bin" "$HOME/bin" "$HOME/.gem/ruby/2.3.0/bin" "$HOME/.gem/ruby/2.4.0/bin" "$GOPATH/bin" "$HOME/.yarn/bin" "$HOME/.npm-global/bin" "$HOME/.cargo/bin" "$ANDROID_HOME/emulator" "$ANDROID_HOME/platform-tools" "$ANDROID_HOME/tools" "/var/lib/snapd/snap/bin"; do
-	if [ -e "$i" ]; then
-		PATH="$i:${PATH}"
-	fi
-done
-export PATH
-
-for i in "${HOME}/.local/share/man" '/usr/share/man'; do
-	if [ -e "$i" ]; then
-		MANPATH="$i:${MANPATH}"
-	fi
-done
-export MANPATH
-
-# PYTHONPATH=~/lib/python;		export PYTHONPATH
-export PYTHONSTARTUP="${HOME}/.pystartup"
-
-# source personal settings from .zshother
-[ -e $HOME/.zshother ] && . ~/.zshother
-
-export XTERM_VERSION='XTerm(327)'
-
-export HIGHLIGHT_OPTIONS='--style=seashell'
-
-export WINIT_HIDPI_FACTOR='1.0'
-
-# vi: ft=zsh:tw=0:sw=4:ts=4
+# export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --hidden --follow --glob "!.git/*"'
+# export FZF_DEFAULT_OPTS='--bind ctrl-z:toggle-all --color=light'
+#
+# # quilt settings, always look for patches in the debian/patches directory
+# export QUILT_PATCHES="debian/patches"
+# export QUILT_DIFF_ARGS="--color=auto"
+#
+# # bc settings
+# export BC_ENV_ARGS="${HOME}/.bcrc"
+#
+# # go lang settings
+# export GOPATH="${HOME}/.local/go"
+#
+# export ANDROID_HOME="${HOME}/Android/Sdk"
+#
+# export LD_LIBRARY_PATH="${HOME}/.local/lib"
+#
+# # set PATH so it includes user's private bin if it exists
+# for i in "$HOME/Documents/toolshed/" "$HOME/.cabal/bin" "$HOME/.local/bin" "$HOME/bin" "$HOME/.gem/ruby/2.3.0/bin" "$HOME/.gem/ruby/2.4.0/bin" "$GOPATH/bin" "$HOME/.yarn/bin" "$HOME/.npm-global/bin" "$HOME/.cargo/bin" "$ANDROID_HOME/emulator" "$ANDROID_HOME/platform-tools" "$ANDROID_HOME/tools" "/var/lib/snapd/snap/bin"; do
+# 	if [ -e "$i" ]; then
+# 		PATH="$i:${PATH}"
+# 	fi
+# done
+# export PATH
+#
+# for i in "${HOME}/.local/share/man" '/usr/share/man'; do
+# 	if [ -e "$i" ]; then
+# 		MANPATH="$i:${MANPATH}"
+# 	fi
+# done
+# export MANPATH
+#
+# # PYTHONPATH=~/lib/python;		export PYTHONPATH
+# export PYTHONSTARTUP="${HOME}/.pystartup"
+#
+# # source personal settings from .zshother
+# [ -e $HOME/.zshother ] && . ~/.zshother
+#
+# # what does that mean?
+# export XTERM_VERSION='XTerm(327)'
+#
+# # TODO for which software?
+# export HIGHLIGHT_OPTIONS='--style=seashell'
+#
+# # don't scale windows on HIDPI devices
+# export WINIT_HIDPI_FACTOR='1.0'
+#
+# # vi: ft=zsh:tw=0:sw=4:ts=4
