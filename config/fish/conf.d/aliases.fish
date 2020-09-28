abfb32856b4b042fbfa47f364a52ca36213c8af0
Jan Christoph Ebersbach <jceb@e-jc.de>
2020-09-28 16:13:15 +0200

    feat(config): add package.json to cdroot configuration

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -130,8 +130,8 @@ function _ch2root --description "jump to the next parent directory containing a 
     return
 end
 
-abbr --add cd. 'cd (_ch2root debian .git .hg .svn)'
-abbr --add cdroot 'cd (_ch2root debian .git .hg .svn)'
+abbr --add cd. 'cd _ch2root debian .git .hg .svn package.json)'
+abbr --add cdroot 'cd (_ch2root debian .git .hg .svn package.json)'
 
 abbr --add cd.. 'cd ..'
 
