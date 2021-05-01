ac742750eddcb3f321922ba66825423f030b9b33
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-05-01 07:51:48 +0200

    feat: update git bindings

--- config/fish/conf.d/git_keybindings.fish
+++ config/fish/conf.d/git_keybindings.fish
@@ -1,7 +1,7 @@
 # inspired by gitnow https://github.com/joseluisq/gitnow
-bind -M insert \es 'echo; if git st; commandline -f repaint; else; end'
-bind -M insert \et 'echo; if tig; commandline -f repaint; else; end'
-bind -M insert \ex 'echo; if tig status; commandline -f repaint; else; end'
-bind -M insert \er 'echo; if git pre; commandline -f repaint; else; end'
-bind -M insert \ep 'echo; if git push; commandline -f repaint; else; end'
-bind -M insert \eg 'echo; if git ls; commandline -f repaint; else; end'
+bind -M insert \es 'echo; if git rev-parse HEAD >/dev/null 2>/dev/null; git st; end; commandline -f repaint'
+bind -M insert \et 'echo; if git rev-parse HEAD >/dev/null 2>/dev/null; tig; end; commandline -f repaint'
+bind -M insert \ex 'echo; if git rev-parse HEAD >/dev/null 2>/dev/null; tig status; end; commandline -f repaint'
+bind -M insert \er 'echo; if git rev-parse HEAD >/dev/null 2>/dev/null; git pre; end; commandline -f repaint'
+bind -M insert \ep 'echo; if git rev-parse HEAD >/dev/null 2>/dev/null; git push; end; commandline -f repaint'
+bind -M insert \eg 'echo; if git rev-parse HEAD >/dev/null 2>/dev/null; git ls; end; commandline -f repaint'
