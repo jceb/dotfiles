aef488c5e4c96ce475e944559bcb25ceaaad0929
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-10-17 07:57:05 +0200

    feat(git): update configuration

--- scm/.config/direnv/lib/ensure_git_identity.sh
+++ scm/.config/direnv/lib/ensure_git_identity.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 
 use_git_identity() {
     # 1: identity
-    find . -type d -name .git -print0 | xargs -0 -n 1 -P 8 "$(dirname "${BASH_SOURCE}")/../set_git_identity.sh" "$1" "${PWD}"
+    find . -maxdepth 3 -type d -name .git -print0 | xargs -0 -n 1 -P 8 "$(dirname "${BASH_SOURCE}")/../set_git_identity.sh" "$1" "${PWD}"
 
     # Synchronous implementation:
     # find . -type d -name .git | while read dir; do
