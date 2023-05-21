0cd76860b8744028ae18b03a00f9470c12a90b53
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-05-21 15:17:17 +0200

    chore: update shebang

--- bin/.local/bin/run-xephyr.sh
+++ bin/.local/bin/run-xephyr.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-if test $# -ge 1
-then
-    Xephyr -screen 1024x768 -ac -noreset $*
+#!/usr/bin/env bash
+if test $# -ge 1; then
+	Xephyr -screen 1024x768 -ac -noreset $*
 else
-    #Xephyr -screen 1398x1033 -ac -noreset :9
-    Xephyr -screen 1024x768 -ac -noreset :9
+	#Xephyr -screen 1398x1033 -ac -noreset :9
+	Xephyr -screen 1024x768 -ac -noreset :9
 fi
