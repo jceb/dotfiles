e634533c584a699cf824a2fa430321297af25852
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-05-31 18:14:45 +0200

    chore: extend git identity support to jj

--- scm/.config/direnv/set_git_identity.sh
+++ scm/.config/direnv/set_git_identity.sh
@@ -3,9 +3,18 @@ set -euo pipefail
 
 # 1: identity
 # 2: current directory
-# 3: path to .git folder in repository
+# 3: relative path to .git folder in repository
 
-cd "$(dirname "${2}/${3}")" && if [ "$(git config user.identity)" != "${1}" ]; then
-    echo "Setting git identity to '$1' for repository ${3}"
-    git identity "$1"
-fi
+(
+    cd "$(dirname "${2}/${3}")" && if [ "$(git config user.identity)" != "${1}" ]; then
+        echo "Setting git identity to '$1' for repository ${3}"
+        git identity "$1"
+    fi
+)
+
+(
+    cd "$(dirname "${2}/${3}/..")" && if [ -d .jj ] && [ "$(jj config get user.identity)" != "${1}" ]; then
+        echo "Setting jj identity to '$1' for repository ${3}"
+        jj-identity "$1"
+    fi
+)
