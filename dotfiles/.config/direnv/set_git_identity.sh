5e7628a82c86cdc23089a905a26501f63e2019a2
Jan Christoph Ebersbach <jceb@e-jc.de>
2026-05-15 09:04:18 +0200

    fix(direnv): remove error messages from jj-identity

--- dotfiles/.config/direnv/set_git_identity.sh
+++ dotfiles/.config/direnv/set_git_identity.sh
@@ -5,16 +5,17 @@ set -euo pipefail
 # 2: current directory
 # 3: relative path to .git folder in repository
 
-(
-    cd "$(dirname "${2}/${3}")" && if [ "$(git config user.identity)" != "${1}" ]; then
-        echo "Setting git identity to '$1' for repository ${3}"
+cd "$(dirname "${2}/${3}/..")"
+
+if git rev-parse --show-toplevel &>/dev/null; then
+    if  [ "$(git config user.identity)" != "${1}" ]; then
+        echo "Setting git identity to '$1' for repository $(dirname "${3}")"
         git identity "$1"
     fi
-)
-
-(
-    cd "$(dirname "${2}/${3}/..")" && if [ -d .jj ] && [ "$(jj config get user.identity)" != "${1}" ]; then
-        echo "Setting jj identity to '$1' for repository ${3}"
-        jj-identity "$1"
+fi
+if jj workspace root &>/dev/null && [ -z  "$(jj workspace root &>/dev/null)" ]; then
+    if [ "$(jj config get user.identity 2>/dev/null)" != "${1}" ]; then
+        echo "Setting jj identity to '$1' for repository $(dirname "${3}")"
+        jj-identity -u "$1"
     fi
-)
+fi
